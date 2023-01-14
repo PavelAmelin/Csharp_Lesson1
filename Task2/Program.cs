afd00515472af7e9d7614157737993673291de93
Pavel <sivec_p@mail.ru>
2023-01-14 17:15:37 +0300

    add a Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a < b)
+{
+Console.Write("max = ");
+Console.WriteLine(b);
+}
+else
+{
+Console.Write("max = ");
+Console.WriteLine(a);
+}
