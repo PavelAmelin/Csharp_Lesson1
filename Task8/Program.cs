a27eb1109aac4b43b927eefc39b271621f3b0cb5
Pavel <sivec_p@mail.ru>
2023-01-14 19:01:37 +0300

    add a Task 8

---
+++ Task8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int k = 1;
+while (k <= N)
+{
+    if (k % 2 == 0)
+        if (k + 1 < N)
+        Console.Write($"{k}, ");
+        else
+        Console.WriteLine($"{k}");
+    k += 1;
+}
+
+
