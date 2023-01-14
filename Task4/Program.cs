9b55c1b4bebab434392070908bc69a8e81026e53
Pavel <sivec_p@mail.ru>
2023-01-14 18:04:19 +0300

    add a Task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    if (a > c)
+    Console.WriteLine(a);
+}
+if (b > a)
+{
+    if (b > c)
+    Console.WriteLine(b);
+}
+if (c > b)
+{
+    if (c > a)
+    Console.WriteLine(c);
+}
